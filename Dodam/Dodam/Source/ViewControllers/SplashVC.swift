f7c02b2fd0506a318f6728b7acd472ca2220dcd1
yangsubinn <cindy010670@gmail.com>
2021-08-17 00:36:41 +0900

    component 추가 및 SplashVC 레이아웃 추가 수정

--- Dodam/Dodam/Source/ViewControllers/SplashVC.swift
+++ Dodam/Dodam/Source/ViewControllers/SplashVC.swift
@@ -11,6 +11,9 @@ import SnapKit
 class SplashVC: UIViewController {
     
     let logoImage = UIImageView()
+    let logoTypoImage = UIImageView()
+    let loginButton = BlueButton(title: "Log in")
+    let signUpButton = LightButton(title: "Sign up")
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,16 +24,39 @@ class SplashVC: UIViewController {
     
     func configUI() {
         logoImage.image = UIImage(named: "logo")
+        logoTypoImage.image = UIImage(named: "logo_typo")
     }
     
     func setupLayout() {
         view.addSubview(logoImage)
+        view.addSubview(loginButton)
+        view.addSubview(logoTypoImage)
+        view.addSubview(signUpButton)
         
         logoImage.snp.makeConstraints { make in
             make.centerX.equalToSuperview().inset(20)
             make.centerY.equalToSuperview()
             make.width.equalTo(104)
             make.height.equalTo(128)
+        }
+        
+        logoTypoImage.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(logoImage.snp.bottom).offset(28)
+            make.width.equalTo(110)
+            make.height.equalTo(36)
+        }
+        
+        loginButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.top.equalTo(logoImage.snp.bottom).offset(50)
+        }
+        
+        signUpButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.top.equalTo(loginButton.snp.bottom).offset(14)
         }
     }
     
